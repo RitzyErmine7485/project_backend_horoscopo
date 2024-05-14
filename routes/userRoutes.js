894c096253b7f73673fae04cf98d416352c6cafb
Ritzy <63694858+RitzyErmine7485@users.noreply.github.com>
2024-05-14 13:07:30 -0600

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router()
 
 const { login, registrar,showdata, updateUsuario ,deleteUsuario} = require('../controllers/userController')
 
-router.get('/login', login)
-router.post('/registrar',registrar)
-router.put('/:id', updateUsuario)
-router.delete('/:id', deleteUsuario)
-router.get('/data', showdata)
+router.get('/login/', login)
+router.post('/registrar/',registrar)
+router.put('/:id/', updateUsuario)
+router.delete('/:id/', deleteUsuario)
+router.get('/data/', showdata)
 
 module.exports = router
